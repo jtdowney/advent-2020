be164536ce0c15e469c3a0ced66466902c94c1f4
John Downey <jdowney@gmail.com>
2020-12-02 07:21:37 -0600

    Cleanup day 2
    Decided to use regex instead of my own parser

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,97 +1,54 @@
 use anyhow::{anyhow, Context, Result};
+use regex::Regex;
 use std::io::{self, BufRead};
-use std::str::FromStr;
 
+type PasswordRule = (usize, usize, char);
 struct Entry {
     rule: PasswordRule,
     password: String,
 }
 
-impl FromStr for Entry {
-    type Err = anyhow::Error;
-
-    fn from_str(entry: &str) -> Result<Self, anyhow::Error> {
-        let mut parts = entry.split(':');
-        let rule = parts
-            .next()
-            .ok_or_else(|| anyhow!("No rule to parse"))
-            .and_then(|rule| rule.parse())?;
-        let password = parts
-            .next()
-            .ok_or_else(|| anyhow!("No password to parse"))?
-            .trim()
-            .to_string();
-
-        Ok(Entry { rule, password })
-    }
+fn part1_policy((minimum, maximum, letter): PasswordRule, password: &str) -> bool {
+    let count = password.chars().filter(|&c| c == letter).count();
+    count >= minimum && count <= maximum
 }
 
-struct PasswordRule {
-    minimum: usize,
-    maximum: usize,
-    letter: char,
-}
-
-impl FromStr for PasswordRule {
-    type Err = anyhow::Error;
-
-    fn from_str(entry: &str) -> Result<Self, anyhow::Error> {
-        let mut parts = entry.split_whitespace();
-        let (minimum, maximum) = parts
-            .next()
-            .ok_or_else(|| anyhow!("No range to parse"))
-            .and_then(|range| {
-                range
-                    .split('-')
-                    .map(|n| n.parse().context("Unable to parse range"))
-                    .collect::<Result<Vec<usize>>>()
-            })
-            .map(|range_parts| (range_parts[0], range_parts[1]))?;
-
-        let letter = parts
-            .next()
-            .and_then(|value| value.chars().next())
-            .ok_or_else(|| anyhow!("No letter to parse"))?;
-
-        Ok(PasswordRule {
-            minimum,
-            maximum,
-            letter,
-        })
-    }
-}
-
-fn part1_policy(rule: &PasswordRule, password: &str) -> bool {
-    let count = password.chars().filter(|&c| c == rule.letter).count();
-    count >= rule.minimum && count <= rule.maximum
-}
-
-fn part2_policy(rule: &PasswordRule, password: &str) -> bool {
+fn part2_policy((first, last, letter): PasswordRule, password: &str) -> bool {
     let chars = password.chars().collect::<Vec<char>>();
-    let a = chars[rule.minimum - 1];
-    let b = chars[rule.maximum - 1];
+    let a = chars[first - 1];
+    let b = chars[last - 1];
 
-    (a == rule.letter) ^ (b == rule.letter)
+    (a == letter) ^ (b == letter)
 }
 
 fn solve<F>(input: &[Entry], policy: F) -> usize
 where
-    F: Fn(&PasswordRule, &str) -> bool,
+    F: Fn(PasswordRule, &str) -> bool,
 {
     input
         .iter()
-        .filter(|entry| policy(&entry.rule, &entry.password))
+        .filter(|entry| policy(entry.rule, &entry.password))
         .count()
 }
 
 fn main() -> Result<()> {
+    let re = Regex::new(r"(\d+)-(\d+) (\w): (\w+)").context("Regex build failed")?;
     let input = io::stdin()
         .lock()
         .lines()
-        .map(|line| line.context("Failed to read line"))
         .map(|line| {
-            line.and_then(|entry| entry.parse())
-                .context("Failed to parse entry")
+            line.context("Failed to read line").and_then(|entry| {
+                let captures = re
+                    .captures(&entry)
+                    .ok_or_else(|| anyhow!("Regex match failed"))?;
+                let a = captures[1].parse()?;
+                let b = captures[2].parse()?;
+                let letter = captures[3].chars().next().unwrap();
+                let password = captures[4].to_string();
+                let rule = (a, b, letter);
+                let entry = Entry { rule, password };
+                Ok(entry)
+            })
         })
         .collect::<Result<Vec<Entry>>>()?;
 

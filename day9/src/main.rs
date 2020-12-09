cada690fafc7291d175a0dbe917030d1cdb472bb
John Downey <jdowney@gmail.com>
2020-12-09 06:11:06 -0600

    Add day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,53 @@
+use std::io::{self, BufRead};
+
+use anyhow::{Context, Result};
+use itertools::Itertools;
+
+const PREAMBLE_SIZE: usize = 25;
+
+fn part1(input: &[u64]) -> u64 {
+    let answer = input
+        .windows(PREAMBLE_SIZE + 1)
+        .find(|window| {
+            let candidates = &window[0..PREAMBLE_SIZE];
+            let sum = window[PREAMBLE_SIZE];
+
+            !candidates
+                .iter()
+                .combinations(2)
+                .any(|numbers| numbers.iter().copied().sum::<u64>() == sum)
+        })
+        .expect("answer");
+    let missing = answer[PREAMBLE_SIZE];
+
+    println!("Part 1: {}", missing);
+
+    missing
+}
+
+fn part2(input: &[u64], sum: u64) {
+    let answer = (2..input.len())
+        .map(|n| input.windows(n))
+        .find_map(|mut windows| windows.find(|window| window.iter().copied().sum::<u64>() == sum))
+        .expect("answer");
+
+    let (min, max) = answer.iter().copied().minmax().into_option().unwrap();
+    let value = min + max;
+    println!("Part 2: {}", value);
+}
+
+fn main() -> Result<()> {
+    let input = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| {
+            line.context("unable to read line")
+                .and_then(|l| l.parse().context("unable to parse line"))
+        })
+        .collect::<Result<Vec<u64>>>()?;
+
+    let part1 = part1(&input);
+    part2(&input, part1);
+
+    Ok(())
+}

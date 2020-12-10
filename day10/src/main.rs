14a4834de497a68ab2e4fae6adbee48955d28e2a
John Downey <jdowney@gmail.com>
2020-12-10 17:23:31 -0600

    Add day 10

---
+++ day10/src/main.rs
@@ -0,0 +1,70 @@
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
+
+use anyhow::{Context, Result};
+
+fn part1(input: &[usize]) {
+    let (ones, threes) = input
+        .iter()
+        .scan(0, |prev, &value| {
+            let diff = value - *prev;
+            *prev = value;
+
+            Some(diff)
+        })
+        .fold((0, 1), |(mut ones, mut threes), difference| {
+            match difference {
+                1 => ones += 1,
+                3 => threes += 1,
+                _ => {}
+            }
+
+            (ones, threes)
+        });
+
+    println!("Part 1: {}", ones * threes);
+}
+
+fn part2(input: &[usize]) {
+    let goal = input.last().unwrap();
+    let mut cache = HashMap::new();
+
+    for &i in input {
+        cache.insert(i, 0);
+
+        if i <= 3 {
+            cache.entry(i).and_modify(|v| *v += 1);
+        }
+
+        let end = i.min(3);
+        let total = (1..=end)
+            .filter_map(|n| {
+                let index = i - n;
+                cache.get(&index)
+            })
+            .sum::<u64>();
+        cache.entry(i).and_modify(|v| *v += total);
+    }
+
+    let answer = cache[goal];
+    println!("Part 2: {}", answer);
+}
+
+fn main() -> Result<()> {
+    let mut input = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| {
+            line.context("unable to read line")
+                .and_then(|n| n.parse().context("unable to parse line"))
+        })
+        .collect::<Result<Vec<usize>>>()?;
+    input.sort_unstable();
+
+    part1(&input);
+    part2(&input);
+
+    Ok(())
+}
